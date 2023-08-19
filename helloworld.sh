0ed4128f995d2966746533be9fc0677c46da7b6d
Shrinevaz <shrinevaz@gmail.com>
2023-08-20 01:02:09 +0530

    kkkk

--- helloworld.sh
+++ helloworld.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
- PERSON1=Ramesh
- PERSON2=Suresh
+ 
 echo  "helloworld"
+PERSON1=Ramesh
+PERSON2=Suresh
+
 echo "$Person1: hi $PERSON2"
 echo "$PERSON2: hi $Person1 how are you"
-echo " $Person1: i am fine"
+echo "$Person1: i am fine"
 echo " $PERSON2: thanks"
