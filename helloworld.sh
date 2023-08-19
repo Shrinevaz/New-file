0d9557f52b0a71e5c74fe06633ce45a3d10ec2a6
Shrinevaz <shrinevaz@gmail.com>
2023-08-20 01:04:05 +0530

    lolol

--- helloworld.sh
+++ helloworld.sh
@@ -4,7 +4,7 @@ echo  "helloworld"
 PERSON1=Ramesh
 PERSON2=Suresh
 
-echo "$Person1: hi $PERSON2"
-echo "$PERSON2: hi $Person1 how are you"
-echo "$Person1: i am fine"
+echo "$PERSON1: hi $PERSON2"
+echo "$PERSON2: hi $PERSON1 how are you"
+echo "$PERSON1: i am fine"
 echo " $PERSON2: thanks"
