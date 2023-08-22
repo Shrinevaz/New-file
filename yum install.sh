b6e9171cb376709320302ded87a0833f8e9c1141
Shrinevaz <shrinevaz@gmail.com>
2023-08-23 00:22:07 +0530

    ff

--- yum install.sh
+++ yum install.sh
@@ -4,8 +4,10 @@ userid=$(id -u)
 
 if [ userid -ne = 0  ]
 then 
-    echo " Error:- "
-else 
-echo " You are a root user"
+    echo " Error:- please run this script with root user "
+    exit 1
+#else 
+#echo " You are a root user"
 
 fi 
+yum install git -y 
